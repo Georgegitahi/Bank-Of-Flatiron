2fbe36fb69a6d88cb347407eb1a475c1faee1272
Georgegitahi <petergeorgembugua724@gmail.com>
2024-04-29 13:42:44 +0300

    Code Challenge Complete

---
+++ src/components/Search.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Search({handleSearch}){
+  const handleChange = (event) => {
+    handleSearch(event.target.value); 
+  }
+  return (
+    <div className="ui large fluid icon input">
+      <input
+        type="text"
+        placeholder="Search your Recent Transactions"
+        onChange={handleChange}
+      />
+      <i className="circular search link icon"></i>
+    </div>
+  );
+}
+
+export default Search;
